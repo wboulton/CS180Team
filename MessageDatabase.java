fcd5f7fe16a0618b1bc6a09d065a67e69bd51cd9
wboulton <wboulton@purdue.edu>
2024-10-25 14:30:31 -0400

    Starting database file

--- MessageDatabase.java
+++ MessageDatabase.java
@@ -1,3 +1,10 @@
+import java.util.*;
+import java.io.*;
+
 public class MessageDatabase {
+    protected ArrayList<Message> recievedMessages;
+    protected ArrayList<Message> sentMessages;
+    private User user; // this will be the user who has sent or recieved the messages stored in said database object
+
     
 }
