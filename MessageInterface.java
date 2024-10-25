01e53ea0f912eded5c018c58400cc6169f63c229
wboulton <wboulton@purdue.edu>
2024-10-25 15:20:58 -0400

    The interfaces may be done??

--- MessageInterface.java
+++ MessageInterface.java
@@ -5,8 +5,9 @@ public interface MessageInterface {
     byte[] getPicture();
     boolean hasPicture();
     String toString();
-    void editMessage();
+    void editMessage(); //these void messages need signatures
     void sendMessage();
     void deleteMessage();
+    void sendPicture();
     void readMessage(); //idk if we are going to use this, the point is to actually open the message for a reciever. May be of type String
 }
