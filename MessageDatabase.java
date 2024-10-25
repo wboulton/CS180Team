cd85a8b12547097bb72cac6b19c61ee30dfc6ba8
wboulton <wboulton@purdue.edu>
2024-10-25 14:34:09 -0400

    Update MessageDatabase.java

--- MessageDatabase.java
+++ MessageDatabase.java
@@ -5,6 +5,20 @@ public class MessageDatabase {
     protected ArrayList<Message> recievedMessages;
     protected ArrayList<Message> sentMessages;
     private User user; // this will be the user who has sent or recieved the messages stored in said database object
+    private String filePath;
 
+    public MessageDatabase(User user, String filePath) {
+        this.user = user;
+        this.filePath = filePath;
+        recievedMessages = new ArrayList<>();
+        sentMessages = new ArrayList<>();
+    }
+    
+    private ArrayList getSentMessages() {
+        return sentMessages;
+    }
+    private ArrayList getRecievedMessages() {
+        return recievedMessages;
+    }
     
 }
