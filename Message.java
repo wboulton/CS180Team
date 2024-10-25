3fa4dda14e76615d8e69d0257427ff88f52c117a
wboulton <wboulton@purdue.edu>
2024-10-25 14:55:57 -0400

    Created constructor for messages

--- Message.java
+++ Message.java
@@ -1,12 +1,17 @@
 public class Message {
-    private User sender;
-    private User reciever;
+    private String sender; //usernames of sender and reciever
+    private String reciever;
     private String content;
     private byte[] pictureContent;
     private boolean containsPicture;
-//this should parse csv of some format, probably: sender,reciever,content,containsPicture,pictureContent, 
+//this should parse csv of some format, probably: sender,reciever,content,containsPicture,pictureContent
     public Message(String data) { 
-
+        String[] info = data.split(",");
+        sender = info[0];
+        reciever = info[1];
+        content = info[2]; //content will be null if there is not text in the message and only a picture presumably
+        containsPicture = Boolean.parseBoolean(info[3]);
+        pictureContent = info[4].getBytes();
     }
 
     public User getReciever() {
