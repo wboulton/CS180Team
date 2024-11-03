ccc2677605e0426d6caea7c711aeb47151380f0e
menonj15 <menonj@purdue.edu>
2024-11-03 10:01:58 -0600

    Create MessageDatabaseTest.java

---
+++ MessageDatabaseTest.java
@@ -0,0 +1,42 @@
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.io.*;
+
+public class MessageDatabaseTest {
+
+   @Test(timeout = 1000)
+      public void testMessageDatabase() throws BadDataException {
+          UserDatabase db = new UserDatabase();
+          User you = db.createUser("you", "password", "first", "last", "false");
+          User me = db.createUser("me", "password", "first", "last", "false");
+          MessageDatabase y = new MessageDatabase(you);
+          MessageDatabase m = new MessageDatabase(me);
+          User u = new User("sender", "password", "first", "last", "false");
+          MessageDatabase md = new MessageDatabase(u);
+          assertEquals(0, md.getRecievedMessages().size());
+          assertEquals(0, md.getSentMessages().size());
+          Message tester = new Message("0|you|me|content|false");
+          y.sendMessage(tester);
+          assertEquals(1, y.getSentMessages().size());
+          assertEquals(1, m.getRecievedMessages().size());
+          m.deleteMessage(tester);
+          assertEquals(0, m.getRecievedMessages().size());
+          assertEquals(0, md.getRecievedMessages().size());
+          md.recoverMessages();
+          assertEquals(0, md.getRecievedMessages().size());
+          assertEquals(true, md.getSentMessages().contains(tester));
+          try {
+              y.sendMessage(new Message("0|you|stranger|content|false"));
+          } catch (Exception e) {
+              Assert.assertEquals("Make sure BadDataException is thrown by sendMessage()",BadDataException.class, e.getClass());
+          }
+          
+      }
+
+    public static void main(String[] args) throws BadDataException {
+        MessageDatabaseTest mdt = new MessageDatabaseTest();
+        mdt.testMessageDatabase();
+    }
+    
+}
