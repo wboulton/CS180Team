b87df17d2c533335b1d0c14b019b58b156922abc
wboulton <wboulton@purdue.edu>
2024-11-08 10:27:39 -0500

    Update MediaServer.java
    Ok I wrote some things that might or might not handle threads correctly. What I would be worried about with this approach is when the client and server are passed to the run method for the thread they do not keep the connection and can no longer interact.

--- MediaServer.java
+++ MediaServer.java
@@ -2,6 +2,40 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 
-public class MediaServer implements ServerInterface, Runnable {
-
+public class MediaServer extends Thread implements ServerInterface {
+    
+    private static void run(Socket client, ServerSocket server) {
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            writer = new PrintWriter(client.getOutputStream()); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        int port;
+        try {
+        port = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.out.println("Enter a valid 4 digit port number");
+            return;
+        }
+        try {
+            ServerSocket server = new ServerSocket(port);
+            final ArrayList<Socket> socket = new ArrayList<>();
+            while (true) {
+                final Socket newSocket = server.accept();
+                if (!socket.contains(newSocket)) {
+                    socket.add(newSocket);
+                    Thread clientThread = new Thread(() -> run(newSocket, server));
+                    clientThread.start();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
 }
