c7c81afc1aa60fd0242e59dae8703a9c3cddfa5b
wboulton <wboulton@purdue.edu>
2024-11-10 19:43:20 -0500

    Message handling
    Media Server Message handling done

--- MediaServer.java
+++ MediaServer.java
@@ -11,58 +11,62 @@ public class MediaServer extends Thread {
         Timer timer = new Timer();
         User user = null;
         AtomicInteger recentID = new AtomicInteger(0);
+        MessageDatabase messageDatabase;
+        User currentlyViewing = null; 
         try {
             final BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
             final PrintWriter writer  = new PrintWriter(client.getOutputStream()); 
             System.out.println("connected");
             String line = reader.readLine();
-            switch (line) {
-                case "login":
-                    while (true) {
-                        //login
-                        String username = reader.readLine();
-                        String password = reader.readLine();
-                        boolean allowed = database.verifyLogin(username, password);
-                        if (allowed) {
-                            user = UserDatabase.getUser(username);
-                            break;
-                        } else {
-                            writer.println("could not log in");
-                            writer.flush();
-                            continue;
-                        }
-                    }
-                    break;
-                case "new user":
+            if (line.equals("login")) {
+                while (true) {
+                    //login
                     String username = reader.readLine();
                     String password = reader.readLine();
-                    String firstname = reader.readLine();
-                    String lastname = reader.readLine();
-                    String pfp = reader.readLine();
-                    user = database.createUser(username, password, firstname, lastname, pfp);
-                    break;
-                default:
-                    return;
+                    boolean allowed = database.verifyLogin(username, password);
+                    if (allowed) {
+                        user = UserDatabase.getUser(username);
+                        messageDatabase = new MessageDatabase(user);
+                        messageDatabase.recoverMessages();
+                        break;
+                    } else {
+                        writer.println("could not log in");
+                        writer.flush();
+                        continue;
+                    }
+                }
+            } else if (line.equals("new user")) {
+                String username = reader.readLine();
+                String password = reader.readLine();
+                String firstname = reader.readLine();
+                String lastname = reader.readLine();
+                String pfp = reader.readLine();
+                user = database.createUser(username, password, firstname, lastname, pfp);
+                messageDatabase = new MessageDatabase(user);
+            } else {
+                return;
             }
-
-            MessageDatabase messageDatabase = new MessageDatabase(user);
+            
             //update DMs periodically
             final int updateDelay = 3000; // 3 seconds
             TimerTask task = new TimerTask() {
                 public void run() {
                     //update messages
+                    messageDatabase.recoverMessages();
                     ArrayList<Message> recievedMessages = messageDatabase.getRecievedMessages();
                     for (Message message: recievedMessages) {
                         if (message.getMessageID() > recentID.get()) {
-                            writer.write(message.toString());
-                            writer.println();
-                            writer.flush();   
-                            recentID.set(message.getMessageID());
+                            if (message.getSender().equals(currentlyViewing.getUsername())) {
+                                writer.write("INCOMING\\|" + message.toString());
+                                writer.println();
+                                writer.flush();   
+                                recentID.set(message.getMessageID());
+                            }
                         }
                     }
                 }
             };
-            timer.scheduleAtFixedRate(task,updateDelay,updateDelay);
+            timer.scheduleAtFixedRate(task, updateDelay, updateDelay);
 
             // this stuff is just in testing state rn
             while (true) {
@@ -78,12 +82,69 @@ public class MediaServer extends Thread {
         }
     }
 
-    public void messageHandling() {
-
+    public void messageHandling(PrintWriter writer, String line, MessageDatabase database, User viewing) {
+//GET_SENT_MESSAGES, GET_RECIEVED_MESSAGES, RECOVER_MESSAGES, SEND_MESSAGE, DELETE_MESSAGE, EDIT_MESSAGE
+        try {
+            String temp = line.split("\\|")[0];
+            String information = line.replace(temp, "").substring(1);
+            Action task = Action.valueOf(temp);
+            switch (task) {
+                case GET_CONVERSATION:
+                    database.recoverMessages();
+                    ArrayList<Message> messages = database.getSentMessages();
+                    for (Message message : database.getRecievedMessages()) {
+                        messages.add(message);
+                    }
+                    messages.sort(Comparator.comparingInt(Message::getMessageID));
+                    for (Message item : messages) {
+                        writer.write(item.toString());
+                        writer.println();
+                        writer.flush();
+                    }
+                    break;
+//public Message(User sender, User reciever, String content)
+                case SEND_MESSAGE:
+                    String[] messageInfo = information.split("\\|");
+                    User sender = UserDatabase.getUser(messageInfo[0]);
+                    User reciever = UserDatabase.getUser(messageInfo[1]);
+                    Message message = new Message(sender, reciever, messageInfo[2]);
+                    database.sendMessage(message);
+                    if (messageInfo.length > 3) {
+                        String[] pictureStrings = messageInfo[4].split(",");
+                        byte[] picture = new byte[pictureStrings.length];
+                        for (int i = 0; i < pictureStrings.length; i++) {
+                            picture[i] = Byte.parseByte(pictureStrings[i]);
+                        }
+                        message.addPicture(picture);
+                    }
+                    break;
+                case DELETE_MESSAGE:
+                    ArrayList<Message> sent = database.getSentMessages();
+                    int id = Integer.parseInt(information.split("\\|")[0]);
+                    for (Message item : sent) {
+                        if (item.getMessageID() == id) {
+                            database.deleteMessage(item);
+                        }
+                    }
+                    break;
+                case EDIT_MESSAGE:
+                    ArrayList<Message> sentMessages = database.getSentMessages();
+                    Message editedMessage = new Message(information);
+                    for (Message item : sentMessages) {
+                        if (item.getMessageID() == editedMessage.getMessageID()) {
+                            database.editMessage(item, editedMessage);
+                        }
+                    }
+                default:
+                    return;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void userHandling() {
-        
+
     }
 
     public static void main(String[] args) {
